984d99c7168653daec572bb9a0986185ee313138
alexyu2234 <102819199+alexyu2234@users.noreply.github.com>
2022-05-14 14:28:32 -0400

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,3 +1,6 @@
-import math
-
-print(pow(4, 6))
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I Love " + celebrity)
